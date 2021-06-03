e19be6849697db56b9ad9a25b2813d9f06ded60f
suhailvs <suhailvs@gmail.com>
2021-06-04 04:26:31 +0530

    updated models

--- myapp/models.py
+++ myapp/models.py
@@ -7,15 +7,27 @@ class User(AbstractUser):
 
 class Keyword(models.Model):
     name = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=300)
+    def __str__(self):
+        return self.name
+
 
 class Job(models.Model):
     title = models.CharField(max_length=300)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at =  models.DateTimeField(auto_now_add=True)
     keywords = models.ManyToManyField(Keyword,through='JobKeywords')
+    candidate = models.ManyToManyField(Candidate)
     def __str__(self):
         return self.title
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.views import generic
 from django.urls import reverse_lazy
-from myapp.models import Job
+from myapp.models import Job, Keyword, Candidate
 
 class JobListView(generic.list.ListView):
     """Return list of all jobs"""
@@ -10,12 +10,31 @@ class JobListView(generic.list.ListView):
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        fields = ['title'] #,'keywords'] #'__all__'
+        fields = '__all__' #['title'] #,'keywords'] #'__all__'
 
 class JobCreateView(generic.edit.CreateView):
     form_class = JobForm
     model = Job
     success_url = reverse_lazy('job:list')
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        # {'csrfmiddlewaretoken': ['BlGXXgfwJW9FTfEBTMeHBaMOuLKyIv6NSfcgccRTvD0puKcwAnEgteMOsHAhYmWH'], 
+        # 'title': ['Software'], 'created_by': ['1'], 'keywords': ['suhail', 'sufail']}
+
+        post = request.POST.copy() # to make it mutable
+
+        post.setlist('keywords',[Keyword.objects.get_or_create(name=k)[0].id for k in request.POST.getlist('keywords')])
+        # or set several values from dict
+        # post.update({'postvar': 'some_value', 'var': 'value'})
+        # or set list
+        # post.setlist('list_var', ['some_value', 'other_value']))
+        print(post)
+
+        # and update original POST in the end
+        request.POST = post
+        return super().post(request, *args, **kwargs)
+
     def form_valid(self, form):
         print(form.instance)
         print(self.request.POST, 'post data')
