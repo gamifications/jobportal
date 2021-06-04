55014f73fdaa87e951dca5e65f8a6f1e0092f082
suhailvs <suhailvs@gmail.com>
2021-06-04 09:11:48 +0530

    added a basic form

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.views import generic
 from django.urls import reverse_lazy
-from myapp.models import Job, Keyword, Candidate
-
+from myapp.models import Job, Keyword, Candidate, JobKeywords
+from django.shortcuts import render
+from django.forms import inlineformset_factory
 class JobListView(generic.list.ListView):
     """Return list of all jobs"""
     model = Job
@@ -10,7 +11,23 @@ class JobListView(generic.list.ListView):
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        fields = '__all__' #['title'] #,'keywords'] #'__all__'
+        fields = ['title'] #['title'] #,'keywords'] #'__all__'
+
+
+def job_view(request):
+    KeywordFormset = inlineformset_factory(Job,JobKeywords,fields=('keyword', 'tags'),extra=5)
+    if request.method == 'POST':
+        print(request.POST)
+        # <QueryDict: 'jobkeywords_set-TOTAL_FORMS': ['5'], 'jobkeywords_set-INITIAL_FORMS': ['0'], 
+        # 'jobkeywords_set-MIN_NUM_FORMS': ['0'], 'jobkeywords_set-MAX_NUM_FORMS': ['1000'], 
+        # 'title': ['Software developer'], 'jobkeywords_set-0-keyword': ['8'], 
+        # 'jobkeywords_set-0-tags': ['python', 'django'], 'jobkeywords_set-1-keyword': [''], 
+        #'jobkeywords_set-2-keyword': [''], 'jobkeywords_set-3-keyword': [''], 'jobkeywords_set-4-keyword': ['']}>
+
+        
+    form = JobForm()
+    formset = KeywordFormset() # (instance=question)
+    return render(request, 'myapp/job_form.html', {'form': form,'formset': formset})
 
 class JobCreateView(generic.edit.CreateView):
     form_class = JobForm
