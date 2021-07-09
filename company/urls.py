ac2802cb8c8163ff0830b73b3f091388e4ce0b70
suhailvs <suhailvs@gmail.com>
2021-07-09 17:02:45 +0530

    fixed #6: now jobseekers can apply jobs through company domain

--- company/urls.py
+++ company/urls.py
@@ -4,11 +4,11 @@ from django.views.generic import RedirectView
 from django.views.generic import TemplateView
 
 from . import views
-
+app_name = 'company'
 urlpatterns = [
-	path('', TemplateView.as_view(template_name="company/company.html"), name='company'),
+	path('', views.companyview, name='company'), #TemplateView.as_view(template_name="company/company.html"), name='company'),
 	path('ajax_datatable/jobs/', views.JobsAjaxDatatableView.as_view(), name="ajax_datatable_jobs"),
-    path('applyjob/', views.applyjob, name='applyjob'),
+    path('apply/<int:pk>/', views.applyjob, name='applyjob'),
 
     # redirect urls such http://microsoft.localhost:8000/account/login/?next=/payment/ 
     # to http://microsoft.localhost:8000/
--- company/views.py
+++ company/views.py
@@ -1,10 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ajax_datatable.views import AjaxDatatableView
+from django.http import Http404
+from django.contrib import messages
+from django_hosts.resolvers import reverse
 
-from myapp.models import Job
+from myapp.models import Job, JobKeywords, Candidate
+from myapp.forms import CandidateForm
 
-def applyjob(request):
-    return render(request,'company/apply_job.html',{'company': request.get_host().split('.')[0]})
+get_company = lambda r: r.split('.')[0]
+
+
+def companyview(request):
+    company = get_company(request.get_host())
+    if not Job.objects.filter(created_by__company=company):
+        raise Http404("Access Denied")
+    return render(request,'company/company.html',{'company': company})
+
+# def applyjob(request,pk):
+#     company = get_company(request.get_host())
+#     job = Job.objects.filter(created_by__company=company, id=pk).first()
+#     if not job:
+#         # if somebody access subdomain that is not a company, eg: http://some.suhail.pw
+#         raise Http404("Access Denied")
+
+#     return render(request,'company/apply_job.html',{'company': company, 
+#         'form':CandidateForm(),'job': job, 
+#         'jobkeywords': JobKeywords.objects.filter(job=job)})
+
+
+def applyjob(request,pk):
+    company = get_company(request.get_host())
+    job = Job.objects.filter(created_by__company=company, id=pk).first()
+    if not job:
+        # if somebody access subdomain that is not a company, eg: http://some.suhail.pw
+        raise Http404("Access Denied")
+
+    if request.method == 'POST':
+
+        form = CandidateForm(request.POST,request.FILES)
+        if form.is_valid():
+            print('form save')
+            candidate = form.save(commit=False)
+            candidate.job = job
+            candidate.save()
+            messages.success(request, 'Job saved with success!')
+            return redirect(f'/apply/{job.pk}/') # reverse('company:applyjob', args=(job.pk,), host='wildcard'))
+            #)
+        else:
+            print('form errr')
+    else:
+        cand = Candidate(job = job)
+        form = CandidateForm(instance=cand)
+
+    return render(request,'company/apply_job.html',{'company': company, 
+        'form':form,'job': job, 
+        'jobkeywords': JobKeywords.objects.filter(job=job)})
+
+
 
 class JobsAjaxDatatableView(AjaxDatatableView):
     model = Job
@@ -16,15 +68,25 @@ class JobsAjaxDatatableView(AjaxDatatableView):
         {'name': 'title', 'visible': True },
         {'name': 'description', 'visible': True, },
         {'name': 'department', 'visible': True, },
+        {'name': 'apply', 'title': 'Apply Now', 'searchable': False, 'orderable': False, },
         {'name': 'category', 'visible': False},  
         # {'name': 'app_label', 'foreign_field': 'content_type__app_label', 'visible': True, },
     ]
 
     def get_initial_queryset(self, request=None):
-    	queryset = self.model.objects.all()
-    	if 'company' in request.REQUEST:
-            company = request.REQUEST.get('company').split('.')[0]
-            print(company)
+        queryset = self.model.objects.all()
+        if 'company' in request.REQUEST:
+            company = get_company(request.REQUEST.get('company'))
             queryset = queryset.filter(created_by__company=company)
-    	
-    	return queryset
+        
+        return queryset
+
+
+    def customize_row(self, row, obj):
+        # https://github.com/morlandi/django-ajax-datatable#id42
+        row['apply'] = """
+            <a style="cursor:pointer" onclick="const id=this.closest('tr').id.substr(4);
+                window.location.replace('/apply/'+id+'/');">
+               Apply Job
+            </a>
+        """
