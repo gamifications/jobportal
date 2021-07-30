57a413fd03ca1daf6474d87c7349ffc548eaeb77
suhailvs <suhailvs@gmail.com>
2021-07-30 18:04:56 +0530

    added logo and cover picture

--- myapp/templatetags/myapp_tags.py
+++ myapp/templatetags/myapp_tags.py
@@ -1,4 +1,5 @@
 import hashlib
+from django.conf import settings
 from django.template.defaultfilters import register
 
 @register.filter
@@ -6,3 +7,8 @@ def gravatar_url(email, size=40):
     # TEMPLATE USE:  {{ email|gravatar_url:150 }}
     email_hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
     return f"https://www.gravatar.com/avatar/{email_hash}?s={size}&d=identicon"
+
+
+@register.simple_tag
+def get_setting(name):
+    return getattr(settings, name, "")
